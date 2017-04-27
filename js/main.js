ed3e3e87fa498d81d2038cbbcd0e67b089b46b75
dccruz <dccoder@icloud.com>
2017-04-27 07:10:13 -0700

    Example of Concatenation

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+
+// Question 1a: Concatenation: combining things in JavaScript
+
+// var string1 = 'Hello ';
+// var string2 = 'my name is ';
+// var string3 = 'DC';
+
+// // // What do you expect the final message to be?
+// var combinedStrings = string1 + string2 + string3;
+// console.log(combinedStrings);
+
+// What happens here? Hello my name is DC
+
+
+// Question 1b:
+// What happens here? Answer 85260 (since 8 is in quotations its treated like a string )
+var num1 = '8';
+var num2 = 52;
+var num3 = 60;
+// // What do you expect the final message to be?
+var combinedNumbers = num1 + num2 + num3;
+console.log(combinedNumbers);
