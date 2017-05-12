9999484a7b68be8a2bc6f6dcf8df1398311ed56f
dccruz <dccoder@icloud.com>
2017-05-12 07:17:18 -0700

    Create an h1 tag and then use innerText to add some text to it.

--- js/main.js
+++ js/main.js
@@ -174,5 +174,9 @@
 
 
 // #9.3 Change that element's background-color.
-document.getElementsByClassName("class1")[0].style.backgroundColor = "red";
+// document.getElementsByClassName("class1")[0].style.backgroundColor = "red";
+
+// #9.5 Create an h1 and then use innerText to add some text to it
+var newH1 = document.createElement("h1");
+newH1.innerText = "test content, added to an h1";
 
