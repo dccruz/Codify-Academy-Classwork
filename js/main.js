159217c1729193f5d3c8da82761b38dfa145882e
dccruz <dccoder@icloud.com>
2017-05-13 00:00:47 -0700

    9.6 In JS: Add that h1 to the div in your HTML.

--- js/main.js
+++ js/main.js
@@ -177,6 +177,9 @@
 // document.getElementsByClassName("class1")[0].style.backgroundColor = "red";
 
 // #9.5 Create an h1 and then use innerText to add some text to it
-var newH1 = document.createElement("h1");
-newH1.innerText = "test content, added to an h1";
+// var newH1 = document.createElement("h1");
+// newH1.innerText = "test content, added to an h1";
+
+// #9.6 Add that h1 to the div in your HTML
+document.getElementById("divId1").appendChild(newH1);
 
