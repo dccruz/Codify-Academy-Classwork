76edcbd4d3ae21af4d5dd363f9c4a63c0e0e9095
dccruz <dccoder@icloud.com>
2017-07-05 13:38:23 -0500

    Exercise 6.2
    In jQuery: Make the image element appear and disappear when you click
    the corresponding button.

---
+++ Week 06/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  // Exercise 6.4
+  $("#hideBtn").click(function() {
+    $("#exercise3 img").hide();
+  });
+  $("#showBtn").click(function() {
+    $("#exercise3 img").show();
+  });
--- Week 06/readme.md
+++ Week 06/readme.md
@@ -1,2 +1,3 @@
 Exercise for Week 6<br>
 	1. In HTML: Add an image and two buttons.
+	2. In jQuery: Make the image element appear and disappear when you click the corresponding button.
