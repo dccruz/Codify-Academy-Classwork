2a72be53d8dc446244764f1e28efba4d75f5e50d
dccruz <dccoder@icloud.com>
2017-07-23 12:12:32 -0700

    Exercise 7.1
    What kind of variable is this?

---
+++ Week 07/js/main.js
@@ -0,0 +1,19 @@
+//Exercise 7.1
+var testOne = "What kind of variable is this?";
+console.log("testOne is a: " + typeof(testOne));
+var testTwo = 256;
+console.log("testTwo is a: " + typeof(testTwo));
+var testThree = ["What", "kind", "of", "variable", "am", "I?"];
+console.log("testThree is a: " + typeof(testThree)); // we know this as an array and will look at objects next week
+var testFour = false;
+console.log("testFour is a: " + typeof(testFour));
+var testFive = [1, 2, 5];
+console.log("testFive is a: " + typeof(testFive)); // we know this as an array and will look at objects next week
+var testSix = 128;
+console.log("testSix is a: " + typeof(testSix));
+var testSeven = "What am I?"
+console.log("testSeven is a: " + typeof(testSeven));
+var testEight = true;
+console.log("testEight is a: " + typeof(testEight));
+var testNine = ["What kind of variable is this?", 2, 5, 19]; // we know this as an array and will look at objects next week
+console.log("testNine is a: " + typeof(testNine));
---
+++ Week 07/readme.md
@@ -0,0 +1,11 @@
+Exercise for Week 7<br>
+	 1. Determine what variable type each one is <br>
+			var testOne = "What kind of variable is this?";<br>
+			var testTwo = 256;<br>
+			var testThree = ["What", "kind", "of", "variable", "am", "I?"];<br>
+			var testFour = false;<br>
+			var testFive = [1, 2, 5];<br>
+			var testSix = 128;<br>
+			var testSeven = "What am I?"<br>
+			var testEight = true;<br>
+			var testNine = ["What kind of variable is this?", 2, 5, 19];<br>
