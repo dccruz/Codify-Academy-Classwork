abf3b17e08b4ad9f9ab947e108af258a122d9264
dccruz <dccoder@icloud.com>
2017-10-23 21:12:23 -0700

    Update

--- Week 10/js/main.js
+++ Week 10/js/main.js
@@ -1,3 +1,4 @@
-//10.1 Create an array with 5 different strings. Log the length of your array
+// 10.1 Create an array with 5 different strings. Log the length of your array
 
 var stringArray = ["chris", "david", "dylan", "nick", "sam"];
+console.log(stringArray);
