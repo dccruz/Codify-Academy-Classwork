a2d35343cf6408a700d22d03aa06555de612e88b
dccruz <dccoder@icloud.com>
2017-10-25 21:14:01 -0700

    Update

--- Week 10/js/main.js
+++ Week 10/js/main.js
@@ -1,7 +1,8 @@
-// 10.1 Create an array with 5 different strings. Log the length of your array
+//  10.1 Create an array with 5 different strings. Log the length of your array
 
 var stringArray = ["chris", "david", "dylan", "nick", "sam"];
 console.log(stringArray);
 //    Log the length of each of the strings in your array.
 for (var i = 0; i < stringArray.length; i++) {
   console.log(stringArray[i].length);
+};
