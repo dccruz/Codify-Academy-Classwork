a079b058aa9910c82d74db6d548ce37e810a49d5
dccruz <dccoder@icloud.com>
2017-10-25 21:13:41 -0700

    Update

--- Week 10/js/main.js
+++ Week 10/js/main.js
@@ -4,4 +4,4 @@ var stringArray = ["chris", "david", "dylan", "nick", "sam"];
 console.log(stringArray);
 //    Log the length of each of the strings in your array.
 for (var i = 0; i < stringArray.length; i++) {
-  
+  console.log(stringArray[i].length);
